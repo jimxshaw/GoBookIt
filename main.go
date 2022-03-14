40e649aec0bd8d0040975425a63efe79107b8e81
Jim Shaw <jimxshaw@gmail.com>
2022-03-13 21:45:58 -0500

    Add ability to accept user input.

--- main.go
+++ main.go
@@ -16,12 +16,19 @@ func main() {
 	fmt.Printf("Get tickets to %v %v here\n", eventName, time.Now().Year())
 
 	var userName string
+	var email string
 	var userTickets int
 
-	// Ask user for name.
-	userName = "James"
-	userTickets = 4
+	// Get user input.
+	fmt.Println("Enter your user name: ")
+	fmt.Scan(&userName)
 
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets to book: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("User %v booked %v tickets. Confirmation will be sent to the email %v.\n", userName, userTickets, email)
 
 }
