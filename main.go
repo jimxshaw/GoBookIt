ff297c5ba73dcbfa09b5c9b22ae1258fae8c944c
Jim Shaw <jimxshaw@gmail.com>
2022-03-13 21:30:14 -0500

    Update syntax.

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 
-	var eventName = "Go Conf"
+	eventName := "Go Conf"
 	const totalTickets = 100
-	var remainingTickets = 100
+	remainingTickets := 100
 
 	fmt.Println("GoBookIt is your premier booking app!")
 	fmt.Printf("There are %v tickets total and %v tickets remaining\n", totalTickets, remainingTickets)
