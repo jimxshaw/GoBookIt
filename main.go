bccccbb611aa2c89057149fc764033bade4f8ce0
Jim Shaw <jimxshaw@gmail.com>
2022-03-13 21:15:08 -0500

    Add user name and user tickets.

--- main.go
+++ main.go
@@ -15,4 +15,13 @@ func main() {
 	fmt.Printf("There are %v tickets total and %v tickets remaining\n", totalTickets, remainingTickets)
 	fmt.Printf("Get tickets to %v %v here\n", eventName, time.Now().Year())
 
+	var userName string
+	var userTickets int
+
+	// Ask user for name.
+	userName = "James"
+	userTickets = 4
+
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+
 }
