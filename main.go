0d3a23dfb9832f8277de03229e88ca6085d1cbdc
Jim Shaw <jimxshaw@gmail.com>
2022-03-13 20:46:17 -0500

    Add variables for tickets and print them out.

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello GoBookIt!")
+
+	var eventName = "Go Conf"
+	const eventTickets = 100
+	var remainingTickets = 100
+
+	fmt.Println("GoBookIt is your premier booking app!")
+	fmt.Println("There are", eventTickets, "tickets total and", remainingTickets, "tickets remaining")
+	fmt.Println("Get tickets to", eventName, "here")
+
 }
