8981b8e272f88850a008b994f4769c5d849f6a4f
Jim Shaw <jimxshaw@gmail.com>
2022-03-14 20:50:36 -0500

    Put user inputs in for loop.

--- main.go
+++ main.go
@@ -11,34 +11,35 @@ func main() {
 	eventName := "Go Conf " + strconv.Itoa(time.Now().Year())
 	const totalTickets = 100
 	var remainingTickets uint = 100
+	var bookings []string
 
 	fmt.Println("GoBookIt is your premier booking app!")
 	fmt.Printf("There are %v tickets total and %v tickets remaining\n", totalTickets, remainingTickets)
 	fmt.Printf("Get tickets to %v here\n", eventName)
 
-	var bookings []string
+	for {
+		var userName string
+		var email string
+		var userTickets uint
 
-	var userName string
-	var email string
-	var userTickets uint
+		// Get user input.
+		fmt.Println("Enter your user name: ")
+		fmt.Scan(&userName)
 
-	// Get user input.
-	fmt.Println("Enter your user name: ")
-	fmt.Scan(&userName)
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
 
-	fmt.Println("Enter your email address: ")
-	fmt.Scan(&email)
+		fmt.Println("Enter number of tickets to book: ")
+		fmt.Scan(&userTickets)
 
-	fmt.Println("Enter number of tickets to book: ")
-	fmt.Scan(&userTickets)
+		remainingTickets = remainingTickets - userTickets
 
-	remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, userName)
 
-	bookings = append(bookings, userName)
+		fmt.Printf("User %v booked %v tickets. Confirmation will be sent to the email %v.\n", userName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, eventName)
 
-	fmt.Printf("User %v booked %v tickets. Confirmation will be sent to the email %v.\n", userName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, eventName)
-
-	fmt.Printf("Current bookings: %v\n", bookings)
+		fmt.Printf("Current bookings: %v\n", bookings)
+	}
 
 }
