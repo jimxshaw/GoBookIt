778daca74e952e6b11f6953d0f593e0faceaad45
Jim Shaw <jimxshaw@gmail.com>
2022-03-14 19:46:58 -0500

    Add remaining tickets logic and refactor formatting.

--- main.go
+++ main.go
@@ -2,22 +2,23 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 )
 
 func main() {
 
-	eventName := "Go Conf"
+	eventName := "Go Conf " + strconv.Itoa(time.Now().Year())
 	const totalTickets = 100
-	remainingTickets := 100
+	var remainingTickets uint = 100
 
 	fmt.Println("GoBookIt is your premier booking app!")
 	fmt.Printf("There are %v tickets total and %v tickets remaining\n", totalTickets, remainingTickets)
-	fmt.Printf("Get tickets to %v %v here\n", eventName, time.Now().Year())
+	fmt.Printf("Get tickets to %v here\n", eventName)
 
 	var userName string
 	var email string
-	var userTickets int
+	var userTickets uint
 
 	// Get user input.
 	fmt.Println("Enter your user name: ")
@@ -29,6 +30,9 @@ func main() {
 	fmt.Println("Enter number of tickets to book: ")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+
 	fmt.Printf("User %v booked %v tickets. Confirmation will be sent to the email %v.\n", userName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, eventName)
 
 }
